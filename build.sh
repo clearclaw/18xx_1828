e7b369d1c3c28afa7cab38cc93f6b9b1723c54cd
J C Lawrence <claw@kanga.nu>
2019-02-10 19:20:12 -0800

    C&P charter

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ do
   xxpaper make 1828-Papers -p ${paper} build/1828-Papers-${paper}-diecut.pdf
   xxpaper make 1828-Papers -p ${paper} -c build/1828-Papers-${paper}-outline.pdf
 done
-for suffix in Map-A4 Map-B2 Map-letter Market-A4 Market-letter \
+for suffix in Map-A4 Map-B2 Map-letter Market-A3 Market-A4 Market-letter \
               Market-tabloid TrackTiles-letter-diecut
 do
   cp ${GAMENAME}-${suffix}.pdf build
--- publish.sh
+++ publish.sh
@@ -6,5 +6,6 @@ GAMENAME=1828
 
 ./build.sh 
 pushd build
+  chmod 644 *
   rsync -ra --delete * claw@kanga.nu:~/public_html/${GAMENAME}/
 popd
