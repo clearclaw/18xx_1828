aa44ebaeb131cc20d083997f2f44f0200de1dec4
J C Lawrence <claw@kanga.nu>
2019-03-02 18:01:55 -0800

    Box covers

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-set -e
+set -ex
 
 GAMENAME=1828
 
 dir_root=`pwd`
 
 mkdir -p build
-rm -f build/* || true # 'f' hides the error message, `true` hides the dir error
+rm -f build/* || true
 
 \rm -f 1828-Map.png 1828-Market.png
 convert -density 150 1828-Map-B2.pdf -quality 90 1828-Map.png
@@ -17,12 +17,12 @@ mv ./${GAMENAME}-Rules.pdf build
 
 cp ${GAMENAME}-Papers.xxp build/
 cp LICENSE build/
-cp masthead.jpg 1828-box-sides.pdf build/
 
 for paper in letter A4
 do
   xxpaper make 1828-Papers -p ${paper} build/1828-Papers-${paper}-diecut.pdf
   xxpaper make 1828-Papers -p ${paper} -c build/1828-Papers-${paper}-outline.pdf
+  pdfunite masthead-${paper}.pdf box-sides-${paper}.pdf build/1828-BoxCovers-${paper}.pdf
 done
 for suffix in Map-A4 Map-B2 Map-letter Market-A3 Market-A4 Market-letter \
               Market-tabloid TrackTiles-letter-diecut TrackTiles-letter-outline
